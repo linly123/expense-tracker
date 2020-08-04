ab58538b81aff1bc619c238b5255c32ff022253d
chloechloe <chloeying84@gmail.com>
2020-08-04 22:29:56 +0800

    Project init

---
+++ app.js
@@ -0,0 +1,19 @@
+const express = require("express");
+const exphbs = require("express-handlebars");
+const bodyParser = require("body-parser");
+const app = express();
+
+//set up express-handlebars
+app.engine("hbs", exphbs({ defaultLayout: "main", extname: ".hbs" }));
+app.set("view engine", "hbs");
+
+//use body-parser
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.get("/", (req, res) => {
+  res.render("index");
+});
+
+app.listen(3000, () => {
+  console.log("This is running on http://localhost:3000");
+});
